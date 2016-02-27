c5d2facc71fabfac830def7c399bbf9d869db122
coder-Zou <382525705@qq.com>
2016-02-27 15:38:10 +0800

    练习

---
+++ zyp/eclipse/src/com/zyp/Test.java
@@ -0,0 +1,15 @@
+package com.zyp;
+
+public class Test {
+	public void pupAge(){
+		int age = 0;
+		age = age + 7;
+		System.out.println("puppy age is :"+age);
+	}
+	public static void main(String[] args) {
+		Test test = new Test();
+		test.pupAge();
+
+	}
+
+}
