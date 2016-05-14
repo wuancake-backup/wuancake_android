c67c58a8bbc344db1d97aee4473ad76678ebb97f
unknown <>
2016-05-14 13:11:24 +0800

    splash添加动画类AlphaAnimation

--- Frostmaki/SplashDemand/app/src/main/java/com/example/frostmaki/splashdemand/logoActivity.java
+++ Frostmaki/SplashDemand/app/src/main/java/com/example/frostmaki/splashdemand/logoActivity.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
 import android.widget.ImageView;
 
 import android.os.Handler;
@@ -25,7 +27,9 @@ private ImageView imageView;
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.logo);
         imageView=(ImageView)findViewById(R.id.logo_Image);
-        //一个延时的子线程
+
+
+        /*//一个延时的子线程
          new Handler().postDelayed(new Runnable(){
              @Override
              public void run() {
@@ -35,6 +39,30 @@ private ImageView imageView;
                  finish();
              }
          },3000);
+         */
+
+        //动画类AlphaAnimation，渐入渐出效果
+        AlphaAnimation alphaAnimation =new AlphaAnimation(0.1f,1.0f);//(开始时刻透明度，结束时刻透明度)0~1
+        alphaAnimation.setDuration(3000);
+        imageView.startAnimation(alphaAnimation);
+        alphaAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                Intent intent=new Intent(logoActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
     }
 }
 
